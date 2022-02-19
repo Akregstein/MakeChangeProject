f30d655a3d610d4b3a4255364051f264bde3f586
Daniel.Kregstein <Akregstein@hotmail.com>
2022-02-19 10:58:54 -0700

    first user inputs

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,10 +1,47 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
+		double price = 0.0;
+		double tender = 0.0;
+		
+		
+		//user prompt price
+		System.out.println("Please enter the price of the item:");
+		price = kb.nextDouble();
+		
+		System.out.println("Please enter the amount of money paid:");
+		tender = kb.nextDouble();
+		
+		
+		
+		
+	
 	}
+	
+	
+
+	
+
 
 }
+
+
+
+//*/
+////User Story #1
+//The user is prompted asking for the price of the item.
+//
+//User Story #2
+//The user is then prompted asking how much money was tendered by the customer.
+//
+//User Story #3
+//Display an appropriate message if the customer provided too little money or the exact amount.
+//
+//User Story #4
+//If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
+///*
